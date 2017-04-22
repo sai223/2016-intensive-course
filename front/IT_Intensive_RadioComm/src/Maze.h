b9ffabaae10725a61b0cee08aa92cc93daf515aa
HongBeom Choi <HongBeom Choi>
2017-04-22 21:37:08 +0900

    할 말이 없다.

--- front/IT_Intensive_RadioComm/src/Maze.h
+++ front/IT_Intensive_RadioComm/src/Maze.h
@@ -1,20 +1,17 @@
-
 /*
  * Maze.h
  *
  * Created: 4/8/2017 6:38:59 PM
  *  Author: credt
  */ 
-
 #include "TheArtist.h"
-
 #ifndef __MAZE_H__ 
 #define __MAZE_H__ 
-
-#define MAZE_RIGHT_WALL_EXIST_DETERMINATE	5.0
-#define MAZE_FRONT_WALL_EXIST_DETERMINATE	8.0
-#define MAZE_RIGHT_DISTANCE_LOWERBOUND		4.5
-#define MAZE_RIGHT_DISTANCE_UPPERBOUND		8.0
+#define MAZE_FRONT_WALL_EXIST_DETERMINATE	10.5
+#define MAZE_RIGHT_DISTANCE_LOWERBOUND		2.5
+#define MAZE_RIGHT_DISTANCE_UPPERBOUND		6.5
+#define MAZE_LEFT_DISTANCE_LOWERBOUND		7.0
+#define MAZE_NOTHING_LOWERBOUND				20.0
 
 enum DIRECTION_STATUS {
 	STRAIGHT, 
@@ -23,17 +20,15 @@ enum DIRECTION_STATUS {
 	STOP, 
 	BACK
 };
-
 struct ultrasonic_distances{
+	enum DIRECTION_STATUS direction;
 	float left_distance;
 	float center_distance;
 	float right_distance;
 }past_distance;
 
 bool strategic_withdrawal;
-
 void artist_init_maze(void);  
 void artist_do_maze (void);  
 void artist_pause_maze(void);
-
 #endif 
