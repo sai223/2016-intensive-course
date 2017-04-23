b32f541e5fe2aa3ec8ccf6e6dce78cf8710e9bcc
Kihong Kim <Kihong Kim>
2017-04-23 14:21:50 +0900

    Maze.c commit

--- front/IT_Intensive_RadioComm/src/Maze.c
+++ front/IT_Intensive_RadioComm/src/Maze.c
@@ -3,7 +3,7 @@
 * maze.c
 *
 * Created: 4/8/2017 6:38:36 PM
-*  Author: credtiger96, Acka, Kyle  
+*  Author: credtiger96, Acka, Kyle
 */
 #include "Maze.h"
 void artist_init_maze (void) {
@@ -12,55 +12,78 @@ void artist_init_maze (void) {
 }
 /*
 void legacy_maze1(void){
-	if(artist_front.center_distance <= MAZE_FRONT_WALL_EXIST_DETERMINATE){
-		strategic_withdrawal = true;
-		artist_front.maze_status = BACK;
+if(artist_front.center_distance <= MAZE_FRONT_WALL_EXIST_DETERMINATE){
+strategic_withdrawal = true;
+artist_front.maze_status = BACK;
+}
+else if(artist_front.right_distance < MAZE_RIGHT_DISTANCE_LOWERBOUND){
+artist_front.maze_status = LEFT;
+}
+else if(artist_front.right_distance > MAZE_RIGHT_DISTANCE_UPPERBOUND){
+artist_front.maze_status = RIGHT;
+}
+else{
+if(strategic_withdrawal){
+strategic_withdrawal = false;
+artist_front.maze_status = LEFT;
+}
+else artist_front.maze_status = STRAIGHT;
+}
+
+artist_print_ultrasonic_value();
+if(artist_front.maze_status == STRAIGHT) printf("%s\n", "STRA\0");
+else if(artist_front.maze_status == LEFT) printf("%s\n", "LEFT\0");
+else if(artist_front.maze_status == RIGHT) printf("%s\n", "RIGH\0");
+else printf("%s\n", "BACK\0");
+}
+*/
+void artist_do_maze (void) {
+	
+	static int count = 1;
+	if (artist_front.maze_status == LEFT || artist_front.maze_status == RIGHT) {
+		count --;
+		if (count <= 0) {
+			count = 1;
+			}else {
+			return;
+		}
 	}
-	else if(artist_front.right_distance < MAZE_RIGHT_DISTANCE_LOWERBOUND){
+	if(past_distance.direction == RIGHT || past_distance.direction == LEFT){
+		artist_front.maze_status = STRAIGHT;
+	}
+	else if((artist_front.center_distance) < MAZE_FRONT_WALL_LOWERBOUND) {
+		if (artist_front.right_distance < artist_front.left_distance)
 		artist_front.maze_status = LEFT;
+		else
+		artist_front.maze_status = RIGHT;
+		count = 2;
+	}
+	else if(artist_front.center_distance < MAZE_FRONT_WALL_EXIST_DETERMINATE){
+		if (artist_front.right_distance < artist_front.left_distance)
+		artist_front.maze_status = LEFT;
+		else
+		artist_front.maze_status = RIGHT;
 	}
 	else if(artist_front.right_distance > MAZE_RIGHT_DISTANCE_UPPERBOUND){
 		artist_front.maze_status = RIGHT;
 	}
-	else{
-		if(strategic_withdrawal){
-			strategic_withdrawal = false;
-			artist_front.maze_status = LEFT;
-		}
-		else artist_front.maze_status = STRAIGHT;
-	}
-	
-	artist_print_ultrasonic_value();
-	if(artist_front.maze_status == STRAIGHT) printf("%s\n", "STRA\0");
-	else if(artist_front.maze_status == LEFT) printf("%s\n", "LEFT\0");
-	else if(artist_front.maze_status == RIGHT) printf("%s\n", "RIGH\0");
-	else printf("%s\n", "BACK\0");
-}
-*/
-void artist_do_maze (void) {
-	if(past_distance.direction == RIGHT || past_distance.direction == LEFT){
+	else if(artist_front.left_distance < MAZE_LEFT_DISTANCE_LOWERBOUND
+	&& artist_front.right_distance < MAZE_RIGHT_DISTANCE_UPPERBOUND){
 		artist_front.maze_status = STRAIGHT;
 	}
-	else if(artist_front.center_distance < MAZE_FRONT_WALL_EXIST_DETERMINATE){
-		artist_front.maze_status = LEFT;
-	}
-	else if(artist_front.left_distance < MAZE_LEFT_DISTANCE_LOWERBOUND
-		&& artist_front.right_distance < MAZE_RIGHT_DISTANCE_UPPERBOUND){
-		artist_front.maze_status = STRAIGHT;		
-	}
-	else if(artist_front.left_distance > MAZE_NOTHING_LOWERBOUND 
-		&& artist_front.right_distance > MAZE_NOTHING_LOWERBOUND){
+	else if(artist_front.left_distance > MAZE_NOTHING_LOWERBOUND
+	&& artist_front.right_distance > MAZE_NOTHING_LOWERBOUND){
 		artist_front.maze_status = STRAIGHT;
 	}
 	else if(artist_front.left_distance < MAZE_LEFT_DISTANCE_LOWERBOUND){
 		artist_front.maze_status = RIGHT;
 	}
-	else if(artist_front.right_distance > MAZE_RIGHT_DISTANCE_UPPERBOUND){
-		artist_front.maze_status = RIGHT;
-	}
+	
+
+	/*
 	else if(artist_front.right_distance < MAZE_RIGHT_DISTANCE_LOWERBOUND){
-		artist_front.maze_status = LEFT;
-	}
+	artist_front.maze_status = LEFT;
+	}*/
 	else{
 		artist_front.maze_status = STRAIGHT;
 	}
@@ -70,16 +93,11 @@ void artist_do_maze (void) {
 	past_distance.center_distance = artist_front.center_distance;
 	past_distance.right_distance = artist_front.right_distance;
 	
-	#ifdef MAZE_DEBUG
-	
-	artist_print_ultrasonic_value();
-	if(artist_front.maze_status == STRAIGHT) printf("%s\n", "STRA\0");
-	else if(artist_front.maze_status == LEFT) printf("%s\n", "LEFT\0");
-	else if(artist_front.maze_status == RIGHT) printf("%s\n", "RIGH\0");
-	else printf("%s\n", "BACK\0");
-	
-	#endif
-	
+	//artist_print_ultrasonic_value();
+	//if(artist_front.maze_status == STRAIGHT) printf("%s\n", "STRA\0");
+	//if(artist_front.maze_status == LEFT) printf("%s\n", "LEFT\0");
+	//else if(artist_front.maze_status == RIGHT) printf("%s\n", "RIGH\0");
+	//else printf("%s\n", "BACK\0");
 	switch (artist_front.maze_status){
 		case STRAIGHT :
 		usart_write_buffer_job(&(artist_front.usart_instance), "mw\0\0\0", MAX_RX_BUFFER_LENGTH);
